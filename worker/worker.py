99c33b1a72315eb055587c22042661716a30c6b7
Emily Wright <km20200@bristol.ac.uk>
2023-12-06 17:55:54 +0000

    added manual acknowledgments

--- worker/worker.py
+++ worker/worker.py
@@ -8,26 +8,6 @@ import infofile # local file containing cross-sections, sums of weights, dataset
 
 tuple_path = "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/" # web address
 
-
-def callback(ch, method, properties, body):
-    body = body.decode("utf-8")
-    print('WORKER Received '+body)
-    prefix_val = body.split()
-    prefix = prefix_val[0]
-    val = prefix_val[1]
-    url = tuple_path+prefix+val+".4lep.root" # file name to open
-    temp = read_file(url,val)
-    temp.append(val)
-    temp = pickle.dumps(temp)
-    channel.basic_publish(exchange='', routing_key='output', body=temp)
-    print(" WORKER Sent " + val)
-
-# General definitions of fraction of data used, where to access the input files
-
-#lumi = 0.5 # fb-1 # data_A only
-#lumi = 1.9 # fb-1 # data_B only
-#lumi = 2.9 # fb-1 # data_C only
-#lumi = 4.7 # fb-1 # data_D only
 lumi = 10 # fb-1 # data_A,data_B,data_C,data_D
 
 fraction = 1.0 # reduce this is if you want the code to run quicker
@@ -140,6 +120,25 @@ params = pika.ConnectionParameters('year_4_project_2-rabbitmq-1')
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 channel.queue_declare(queue='filestring')
-channel.basic_consume(queue='filestring', auto_ack=True, on_message_callback=callback)
+
+# Set QoS to limit the number of unacknowledged messages to 1
+channel.basic_qos(prefetch_count=1)
+
+def callback(ch, method, properties, body):
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+    body = body.decode("utf-8")
+    print('WORKER Received '+body)
+    prefix_val = body.split()
+    prefix = prefix_val[0]
+    val = prefix_val[1]
+    url = tuple_path+prefix+val+".4lep.root" # file name to open
+    temp = read_file(url,val)
+    temp.append(val)
+    temp = pickle.dumps(temp)
+    channel.basic_publish(exchange='', routing_key='output', body=temp)
+    print(" WORKER Sent " + val)
+
+channel.basic_consume(queue='filestring', auto_ack=False, on_message_callback=callback)
 channel.queue_declare(queue='output')
 channel.start_consuming()
